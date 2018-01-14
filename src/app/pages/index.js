a5724269b1e6a9e15cdcfba29066537bbf0f881a
John Apostol <notimetobeidle@gmail.com>
2018-01-14 03:45:13 -0600

    Add team logos

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -2,9 +2,22 @@
 
 import React from 'react';
 import Layout from '../components/layout';
+import TeamLogo from '../components/teamLogo';
 
 export default () => (
   <Layout>
     Overwatch League Fantasy
+    <TeamLogo team="BOS" />
+    <TeamLogo team="DAL" />
+    <TeamLogo team="FLA" />
+    <TeamLogo team="GLA" />
+    <TeamLogo team="HOU" />
+    <TeamLogo team="LDN" />
+    <TeamLogo team="NYE" />
+    <TeamLogo team="PHI" />
+    <TeamLogo team="SEO" />
+    <TeamLogo team="SFS" />
+    <TeamLogo team="SHD" />
+    <TeamLogo team="VAL" />
   </Layout>
 );
