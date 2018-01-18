6e1405fa593c22109d55b1b7c22912a0cf13d7ce
John Apostol <notimetobeidle@gmail.com>
2018-01-18 00:23:38 -0600

    Address FOUC

---
+++ src/app/pages/_document.js
@@ -0,0 +1,29 @@
+// @flow
+
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+import flush from 'styled-jsx/server';
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }: {renderPage: Function}) {
+    const {
+      html, head, errorHtml, chunks,
+    } = renderPage();
+    const styles = flush();
+    return {
+      html, head, errorHtml, chunks, styles,
+    };
+  }
+
+  render() {
+    return (
+      <html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
