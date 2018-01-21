b3b7418dfd16cbccab67f741b227d75ec2df1f2f
John Apostol <notimetobeidle@gmail.com>
2018-01-21 14:58:20 -0600

    Add proper state to verifyAccount

--- src/functions/index.js
+++ src/functions/index.js
@@ -8,12 +8,13 @@ const handle = app.getRequestHandler();
 const nextApp = https.onRequest((req, res) => {
   // eslint-disable-next-line no-console
   console.log(`File: ${req.originalUrl}`);
-  switch (req.originalUrl) {
+  const url = req.originalUrl.split('?').shift();
+  switch (url) {
     // Handle auth email actions
     case '/auth': {
       const { query: { mode, oobCode } } = req;
       return app.prepare().then(() =>
-        handle(req, res, '/login', { mode, oobCode }));
+        res.redirect(`/login?mode=${mode}&oobCode=${oobCode}`));
     }
     default:
       return app.prepare().then(() => handle(req, res));
