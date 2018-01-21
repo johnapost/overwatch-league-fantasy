60e4d094c47722fb82bf58d72964bbbf5d2e5382
John Apostol <notimetobeidle@gmail.com>
2018-01-21 14:58:20 -0600

    Add proper state to verifyAccount

--- src/app/components/verifyAccount.js
+++ src/app/components/verifyAccount.js
@@ -1,43 +1,67 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
+import { isLoaded } from 'react-redux-firebase';
 import { Card, Button, message } from 'antd';
+import withFirestore from '../shared/withFirestore';
 
 type Props = {
-  currentUser?: {
-    email: string,
+  firestore: Object
+}
+
+type State = {
+  title: string,
+  currentUser: {
     sendEmailVerification: () => Promise<*>
+  } | null
+}
+
+class VerifyAccount extends Component<Props, State> {
+  state = {
+    title: '',
+    currentUser: null,
+  }
+
+  async componentWillMount() {
+    const { firestore } = this.props;
+    if (!isLoaded(firestore)) {
+      return;
+    }
+
+    const auth = await firestore.auth();
+    this.setState({
+      title: auth.currentUser.email,
+      currentUser: auth.currentUser,
+    });
+  }
+
+  handleVerify = () => {
+    if (this.state.currentUser) {
+      this.state.currentUser.sendEmailVerification().then(() => {
+        message.success('Email sent, check your inbox!');
+      }).catch(({ code, message: errorMessage }) => {
+        message.error(errorMessage);
+        // eslint-disable-next-line no-console
+        console.error(code, errorMessage);
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Card title={this.state.title}>
+        Your email hasn&#39;t been verified. Please verify it to proceed.
+        <div style={{ marginTop: '10px' }}>
+          <Button
+            onClick={this.handleVerify}
+            disabled={!this.state.currentUser}
+          >
+            Send a verification email
+          </Button>
+        </div>
+      </Card>
+    );
   }
 }
 
-const VerifyAccount = ({ currentUser }: Props) => (
-  <Card title={currentUser && `Verify ${currentUser.email}`}>
-    Your email hasn&#39;t been verified. Please verify it to proceed.
-    <div style={{ marginTop: '10px' }}>
-      <Button
-        onClick={() => {
-          if (currentUser) {
-            currentUser.sendEmailVerification().then(() => {
-              message.success('Email sent, check your inbox!');
-            })
-            .catch((error) => {
-              message.error(error);
-            });
-          }
-        }}
-        disabled={!currentUser}
-      >
-        Send a verification email
-      </Button>
-    </div>
-  </Card>
-);
-
-VerifyAccount.defaultProps = {
-  currentUser: {
-    email: '',
-    sendEmailVerification: () => Promise.resolve(''),
-  },
-};
-
-export default VerifyAccount;
+export default withFirestore()(VerifyAccount);
