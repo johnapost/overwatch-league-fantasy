c4be37a1596cc1fcec818c2acfa3cf3263085dcf
John Apostol <notimetobeidle@gmail.com>
2018-05-20 18:04:07 -0500

    Show roster (#13) (#14)
    * Add static API data
    
    * Update homepage text
    
    * Add rudimentary roster displaygs
    
    * Display player cards
    
    * Add prettier
    
    * Style Player component

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -5,6 +5,7 @@ import { Row, Col, Card } from 'antd';
 import withRedux from 'next-redux-wrapper';
 import store from '../shared/makeStore';
 import Layout from '../components/layout';
+import Roster from '../components/roster';
 
 const Index = () => (
   <Layout>
@@ -13,9 +14,14 @@ const Index = () => (
       <Col sm={12} md={16}>
         <div className="wrapper">
           <Card>
-            Placeholder card for Overwatch League Fantasy explanation
+            Create or join leagues to compete with your own Overwatch team!
           </Card>
         </div>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <Roster />
       </Col>
     </Row>
     <style jsx>{`
