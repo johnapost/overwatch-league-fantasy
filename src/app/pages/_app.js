d32a7d00aa94fc72939ac0e548ce3c51e020de4e
John Apostol <notimetobeidle@gmail.com>
2018-05-20 20:12:38 -0500

    Upgrade all dependencies

---
+++ src/app/pages/_app.js
@@ -0,0 +1,32 @@
+// @flow
+
+import React from "react";
+import { Provider } from "react-redux";
+import App, { Container } from "next/app";
+import withRedux from "next-redux-wrapper";
+import makeStore from "../shared/makeStore";
+
+export default withRedux(makeStore, { debug: true })(
+  class MyApp extends App {
+    static async getInitialProps({ Component, ctx }) {
+      return {
+        pageProps: {
+          ...(Component.getInitialProps
+            ? await Component.getInitialProps(ctx)
+            : {})
+        }
+      };
+    }
+
+    render() {
+      const { Component, pageProps, store } = this.props;
+      return (
+        <Container>
+          <Provider store={store}>
+            <Component {...pageProps} />
+          </Provider>
+        </Container>
+      );
+    }
+  }
+);
