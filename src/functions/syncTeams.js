5a16e80cbadb7e382b354a39892db88559b0bc0f
John Apostol <notimetobeidle@gmail.com>
2018-12-16 14:06:32 -0600

    Sync firestore teams with overwatch API

--- src/functions/syncTeams.js
+++ src/functions/syncTeams.js
@@ -1,5 +1,30 @@
 // @flow
 
 import { https } from "firebase-functions";
+import { firestore } from "firebase-admin";
+import { get } from "axios";
 
-export default https.onRequest((req, res) => res.send("Yay"));
+const OVERWATCH_API = "https://api.overwatchleague.com/";
+
+const db = firestore();
+db.settings({ timestampsInSnapshots: true });
+
+export default https.onRequest(async (req, res) => {
+  const {
+    data: { competitors }
+  } = await get(`${OVERWATCH_API}teams`);
+
+  try {
+    await competitors.map(({ competitor }) =>
+      db
+        .collection("teams")
+        .doc(String(competitor.id))
+        .set(competitor)
+    );
+    await res.send("Teams synced!");
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("Error syncing teams", error);
+    await res.send(error);
+  }
+});
