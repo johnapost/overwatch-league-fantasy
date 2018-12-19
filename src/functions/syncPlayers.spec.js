136de5719ad48b9033269860416717cfd5cb67cb
John Apostol <notimetobeidle@gmail.com>
2018-12-18 23:50:57 -0600

    Add test for successfully syncing player data

--- src/functions/syncPlayers.spec.js
+++ src/functions/syncPlayers.spec.js
@@ -8,13 +8,33 @@ jest.mock("axios", () => ({ get: jest.fn() }));
 
 describe("syncPlayers", () => {
   const sendSpy = jest.fn();
+  const setSpy = jest.fn();
   const consoleErrorSpy = jest.fn();
 
   beforeEach(() => {
     // eslint-disable-next-line no-console
     console.error = consoleErrorSpy;
     sendSpy.mockReset();
+    setSpy.mockReset();
     consoleErrorSpy.mockReset();
+  });
+
+  it("should sync player data", async () => {
+    firestore.mockImplementation(() => ({
+      batch: () => ({ commit: jest.fn(), set: setSpy }),
+      collection: () => ({ doc: jest.fn() })
+    }));
+    get.mockImplementation(() => ({
+      data: { data: [{ playerId: 1 }, { playerId: 2 }, { playerId: 3 }] }
+    }));
+    const req = {};
+    const res = {
+      send: sendSpy
+    };
+    await syncPlayers(req, res);
+    expect(setSpy).toHaveBeenCalledTimes(3);
+    expect(sendSpy).toHaveBeenCalledTimes(1);
+    expect(sendSpy).toHaveBeenCalledWith("Player data synced!");
   });
 
   it("should send errors", async () => {
