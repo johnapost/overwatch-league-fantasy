a9e4c5b40dbe6092b45c471618b842b4543c44a6
John Apostol <notimetobeidle@gmail.com>
2019-02-18 23:34:31 -0600

    Remove hardcoded week

--- src/functions/syncPlayers.js
+++ src/functions/syncPlayers.js
@@ -4,7 +4,7 @@ import { https } from "firebase-functions";
 import { firestore } from "firebase-admin";
 import { get } from "axios";
 import { PLAYER_API } from "./endpoints";
-// import getWeek from "./shared/getWeek";
+import getWeek from "./shared/getWeek";
 
 const season = "2019";
 const stage = 0;
@@ -17,8 +17,7 @@ export default https.onRequest(async (req, res) => {
 
   try {
     const batch = db.batch();
-    // const week = getWeek(Date.now(), season, stage);
-    const week = 0;
+    const week = getWeek(Date.now(), season, stage);
     if (week === null) throw new Error("No week found! Can't save stats!");
     data.forEach(playerStats => {
       const { playerId } = playerStats;
