122ff11a84c87d6c6bd62ca12a91dbb40400d36a
John Apostol <notimetobeidle@gmail.com>
2019-03-02 22:51:31 -0600

    Move Layout render into App component

--- src/app/pages/leagues.js
+++ src/app/pages/leagues.js
@@ -1,13 +1,12 @@
 // @flow
 
 import React from "react";
-import Layout from "../components/layout";
 import LeaguesList from "../components/leaguesList";
 import RosterGrid from "../components/rosterGrid";
 
 export default () => (
-  <Layout>
+  <>
     <LeaguesList />
     <RosterGrid />
-  </Layout>
+  </>
 );
