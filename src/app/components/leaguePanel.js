d0c112e91888d1f51d15de9a08bbdc088c22137b
John Apostol <notimetobeidle@gmail.com>
2019-03-03 20:30:08 -0600

    Create invite button

--- src/app/components/leaguePanel.js
+++ src/app/components/leaguePanel.js
@@ -3,36 +3,49 @@
 import React from "react";
 import { compose } from "redux";
 import { connect } from "react-redux";
-import { List } from "antd";
+import { List, Icon, Button, Divider } from "antd";
 import withFirestore from "../shared/withFirestore";
 
 import type { StoreState } from "../shared/makeStore";
 import type { User } from "../shared/user";
+import type { League } from "../shared/league";
 
 type Props = {
   id: string,
+  league: League,
+  uid: string,
   users: [string, User][] | null
 };
 
-export const LeaguePanelComponent = ({ id, users }: Props) =>
-  users && (
-    <List
-      bordered
-      dataSource={users}
-      itemLayout="vertical"
-      renderItem={([userId, user]) => (
-        <>
-          <List.Item key={`${id}-${userId}`}>
-            <List.Item.Meta
-              title="Team Name"
-              description={`Managed by: ${user.displayName}`}
-            />
-            Roster goes here
-          </List.Item>
-        </>
-      )}
-    />
-  );
+export const LeaguePanelComponent = ({ id, league, uid, users }: Props) => (
+  <>
+    {uid === league.ownerUser && (
+      <Button type="primary">
+        <Icon type="user-add" />
+        Invite people
+      </Button>
+    )}
+    <Divider />
+    {users && (
+      <List
+        bordered
+        dataSource={users}
+        itemLayout="vertical"
+        renderItem={([userId, user]) => (
+          <>
+            <List.Item key={`${id}-${userId}`}>
+              <List.Item.Meta
+                title="Team Name"
+                description={`Managed by: ${user.displayName}`}
+              />
+              Roster goes here
+            </List.Item>
+          </>
+        )}
+      />
+    )}
+  </>
+);
 
 const filterLeagueUsers = (
   users: {
@@ -53,11 +66,13 @@ export const mapStateToProps = (
   {
     firestore: {
       data: { users }
-    }
+    },
+    user: { uid }
   }: StoreState,
   { id }: Props
 ) => ({
-  users: filterLeagueUsers(users, id)
+  users: filterLeagueUsers(users, id),
+  uid
 });
 
 export default compose(
